1f289f29e68c28d83d793de34af337a571e464f0
Steve Brownlee <stevebrownlee@users.noreply.github.com>
2023-08-09 14:45:59 -0500

    Add foreign key to HairStyle on Customer

--- maneapi/models/customer.py
+++ maneapi/models/customer.py
@@ -7,4 +7,5 @@ class Customer(models.Model):
     """Customer model class"""
     stylist = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
+    style = models.ForeignKey("HairStyle", on_delete=models.CASCADE)
     date_created = models.DateField(auto_now=True)
